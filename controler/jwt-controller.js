61164780da5845842f92e5dc3c381206e60ec3b4
ManishMewada <manish.mewada@blobstation.com>
2023-04-24 01:53:13 +0530

    update the jwt-controller for the name of the token come from req.body

--- controler/jwt-controller.js
+++ controler/jwt-controller.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
-const Token = require( '../models/token');
+const Token = require('../models/token');
 
 dotenv.config();
 
@@ -23,8 +23,7 @@ const authenticateToken = (req, res, next) => {
 }
 
 const createNewToken = async (req, res) => {
-    const refreshToken = req.body.token.split(' ')[1];
-
+    const refreshToken = req.body.refreshToken.split(' ')[1];
     if (!refreshToken) {
         return res.status(401).json({ msg: 'Refresh token is missing' })
     }
@@ -39,10 +38,10 @@ const createNewToken = async (req, res) => {
         if (error) {
             res.status(500).json({ msg: 'invalid refresh token' });
         }
-        const accessToken = jwt.sign({id: user.id}, process.env.ACCESS_SECRET_KEY, { expiresIn: "5m" });
+        const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_SECRET_KEY, { expiresIn: "5m" });
 
         return res.status(200).json({ accessToken: accessToken })
     })
 }
 
-module.exports = {authenticateToken,createNewToken}
+module.exports = { authenticateToken, createNewToken }
